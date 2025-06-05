af8195bcc4a083b4ed81fa61ba7833609c1da3b6
srinivas987 <srinivasreddysangana@gmail.com>
2025-06-05 13:43:05 +0530

    framework commit

---
+++ tests/login.spec.js
@@ -0,0 +1,65 @@
+import { test, expect } from '@playwright/test';
+
+test("Verify login with valid credentials", async ({ page }) => {
+
+    //Actions
+  await page.goto("https://www.saucedemo.com/v1/")
+
+  await page.locator("input[data-test='username']").fill("standard_user")
+
+  await page.locator("input[type='password']").fill("secret_sauce")
+
+  await page.locator("input[type='submit']").click()
+
+  //Assertions
+  await expect(page).toHaveURL("https://www.saucedemo.com/v1/inventory.html")
+
+})
+
+test("Verify login with Valid username and Invalid password", async ({ page }) => {
+
+    //Actions
+  await page.goto("https://www.saucedemo.com/v1/")
+
+  await page.locator("input[data-test='username']").fill("standard_user")
+
+  await page.locator("input[type='password']").fill("test")
+
+  await page.locator("input[type='submit']").click()
+
+  //Assertions
+  await expect(page.locator("//h3[contains(.,'Epic sadface: Username and password do not match any user in this service')]")).toBeVisible()
+
+})
+
+test("Verify login with InValid username and valid password", async ({ page }) => {
+
+    //Actions
+  await page.goto("https://www.saucedemo.com/v1/")
+
+  await page.locator("input[data-test='username']").fill("test")
+
+  await page.locator("input[type='password']").fill("secret_sauce")
+
+  await page.locator("input[type='submit']").click()
+
+  //Assertions
+  await expect(page.locator("//h3[contains(.,'Epic sadface: Username and password do not match any user in this service')]")).toBeVisible()
+
+})
+
+test("Verify login with InValid username and Invalid password", async ({ page }) => {
+
+    //Actions
+  await page.goto("https://www.saucedemo.com/v1/")
+
+  await page.locator("input[data-test='username']").fill("test")
+
+  await page.locator("input[type='password']").fill("test")
+
+  await page.locator("input[type='submit']").click()
+
+  //Assertions
+  await expect(page.locator("//h3[contains(.,'Epic sadface: Username and password do not match any user in this service')]")).toBeVisible()
+
+})
